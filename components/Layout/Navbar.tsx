578bb951d2b5fb6439620fb1ef83b7ba69e52b58
gagal18 <gagaleskibojan@gmail.com>
2023-07-10 11:06:51 +0200

    fix:  next.config

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
         <div className={"h-20 flex mx-auto bg-black w-full max-w-[1360px]"}>
           <div className={"hidden w-full lg:flex justify-between"}>
             <Link href={"/"} className={"flex items-center px-[10px]"}>
-                  <Image className={"w-12"} src={"/assets/icon.png"}  alt={"Bojan Gagaleski"}/>
+                  <Image className={"w-12"} src={`${process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY : ''}/assets/icon.png`}  alt={"Bojan Gagaleski"}/>
             </Link>
             <div className={"flex items-center gap-[10px]"}>
                 <NavbarItem to={"/"} name={"home"}/>
@@ -27,7 +27,7 @@ function Navbar() {
               }
           >
             <div className={"flex items-center"}>
-              <Image className={"w-12"} src={"/assets/icon.png"}  alt={"Bojan Gagaleski"}/>
+              <Image className={"w-12"} src={`${process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY : ''}/assets/icon.png`}  alt={"Bojan Gagaleski"}/>
             </div>
             {!toggleNavbar ? (
                 <div
