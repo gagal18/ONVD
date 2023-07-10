92bee2b6527cd4f803797960ddaba4eda205a990
gagal18 <gagaleskibojan@gmail.com>
2023-07-10 10:51:14 +0200

    fix: assets path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const nextConfig = {
   trailingSlash: true,
   output: 'export',
   publicRuntimeConfig: {
-    basePath: process.env.NODE_ENV === 'production' ? '/portfolio-revamp' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    assetsPath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   },
 }
 module.exports = nextConfig
