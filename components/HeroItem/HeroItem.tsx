00e6948e162cd4ce2e7fb26d5e941c87bebc36e7
gagal18 <gagaleskibojan@gmail.com>
2023-07-10 21:36:19 +0200

    text

--- components/HeroItem/HeroItem.tsx
+++ components/HeroItem/HeroItem.tsx
@@ -14,9 +14,9 @@ const HeroItem: React.FC<HeroItemProps> = ({ header,  children}) => {
     const handleMouseLeave = () => {
         gsap.to(scaleRef.current, { scale: 1,  duration: 0.3 });
     };    return (
-        <div ref={scaleRef} onMouseLeave={handleMouseLeave} onMouseOver={handleMouseOver} className="shadow-left shadow-[#f0f0f04d] rounded-r-lg p-[20px] flex flex-col justify-between min-h-[250px]  min-w-[550px] bg-black last:mb-[200px]">
+        <div ref={scaleRef} onMouseLeave={handleMouseLeave} onMouseOver={handleMouseOver} className="shadow-left shadow-[#f0f0f04d] rounded-r-lg p-[20px] flex flex-col justify-between min-h-[250px]  max-w-[750px] bg-black last:mb-[200px]">
             <div className="text-white text-[24px]">{header}</div>
-            <div className="text-white">{children}</div>
+            {children}
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,19 +11,35 @@ const Home: React.FC = () => {
             <div className="max-w-[1360px] mx-auto relative flex h-min pt-[100px] pb-[150px]">
                     <div className="left-0 absolute flex flex-col gap-[40px]">
                         <HeroItem header={"welcome!"} >
-                            <div>01</div>
+                            <div className={"text-white-off flex"}>
+                                <span>01</span>
+                            </div>
                         </HeroItem>
                         <HeroItem header={"who is Bojan:"} >
-                            <div>02</div>
+                            <div className={"text-white-off flex flex-col gap-[10px]"}>
+                                <div>Bojan is 20 years old, Front-End Developer at Eagle IT Solutions, Student at FCSE - Skopje(majoring Computer Science)</div>
+                                <span>02</span>
+                            </div>
                         </HeroItem>
                         <HeroItem header={"as Developer"} >
-                            <div>03</div>
+                            <div className={"text-white-off flex flex-col gap-[10px]"}>
+                                <div>In love with React.JS, making sure I write better code everyday</div>
+                                <div>In love with React.JS, making sure I write better code everyday</div>
+                                <span>03</span>
+                            </div>
                         </HeroItem>
                         <HeroItem header={"as Student"} >
-                            <div>04</div>
+                            <div className={"text-white-off flex flex-col gap-[10px]"}>
+                                <div></div>
+                                <span>04</span>
+                            </div>
                         </HeroItem>
                         <HeroItem header={"as Young Individual"} >
-                            <div>05</div>
+                            <div className={"text-white-off flex flex-col gap-[10px]"}>
+                                <div>Running and Exercising to stay healthy and active, while maintaining full-time job and being full-time student</div>
+                                <div>Reading<span className={"text-xs"}>(slow)</span> and Partying<span className={"text-xs"}>(hard)</span> occasionally to keep everything under-control</div>
+                                <span>05</span>
+                            </div>
                         </HeroItem>
                     </div>
                 </div>
