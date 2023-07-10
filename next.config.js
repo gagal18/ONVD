1ff53354a9882d8f1c539ad2eeb223e9c74e990a
gagal18 <gagaleskibojan@gmail.com>
2023-07-10 11:03:15 +0200

    fix:  next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   publicRuntimeConfig: {
     basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-    assetsPath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
   },
 }
 module.exports = nextConfig
