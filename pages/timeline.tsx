328411124a26c96d870898ae0a8f80fdb2f5ae33
gagal18 <gagaleskibojan@gmail.com>
2023-07-10 12:57:03 +0200

    fix

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Heading from "../components/Heading/Heading";
+import 'react-vertical-timeline-component/style.min.css';
+import TimelineSection from "../components/Timeline/TimelineSection";
 
 const Timeline: React.FC = () => {
     return (
         <div className={"w-full mx-auto relative"}>
-            <div className="max-w-[1360px] mx-auto relative flex h-min pt-[100px] pb-[150px]">
+            <div className="max-w-[1360px] mx-auto relative flex flex-col h-min pt-[100px] pb-[150px]">
                 <Heading specChar={"."} text={"my journey"}/>
+                <TimelineSection />
             </div>
         </div>
     )
